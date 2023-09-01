2e963e46c568dd79d4a24275a41bc4ff4e777828
Yash Bhardwaj <86493014+yashB-2002@users.noreply.github.com>
2023-09-01 17:01:23 +0530

    Update CartPage.js

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -2,6 +2,8 @@ import React from "react";
 import Layout from "../components/Layout";
 import { useAuth } from "../context/auth";
 import { useCart } from "../context/Context";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
 
 const CartPage = () => {
   const [auth] = useAuth();
@@ -11,6 +13,19 @@ const CartPage = () => {
     let newCart = cart.filter((p) => p._id !== product._id);
     setCart(newCart);
     localStorage.setItem("cart", JSON.stringify(newCart));
+  };
+  const makePayment = async () => {
+    console.log(cart);
+    const stripe = await loadStripe("your stripe private key");
+    const body = {
+      products: cart,
+    };
+    const { data } = await axios.post("/api/v1/product/checkout", body);
+    console.log(data);
+    const result = stripe.redirectToCheckout({
+      sessionId: data.id,
+    });
+    if (result.error) console.log(result.error);
   };
   return (
     <Layout>
@@ -42,10 +57,16 @@ const CartPage = () => {
             <div className="checkout-section">
               <h2 className="cart-heading">Proceed to Checkout</h2>
               <p>
+                Total Prdoucts:{" "}
+                <span style={{ fontWeight: "bold" }}> {cart?.length}</span>
+              </p>
+              <p>
                 Subtotal:{" "}
                 <span style={{ fontWeight: "bold" }}>₹{totalCartPrice}</span>
               </p>
-              <button className="checkout-button">Proceed to Checkout</button>
+              <button className="checkout-button" onClick={makePayment}>
+                Proceed to Checkout
+              </button>
             </div>
           </div>
         </div>
