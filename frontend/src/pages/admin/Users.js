36815df45c979b6bfd5e28356da4ff797bc50c84
Yash Bhardwaj <86493014+yashB-2002@users.noreply.github.com>
2023-09-02 20:34:10 +0530

    Update Users.js

--- frontend/src/pages/admin/Users.js
+++ frontend/src/pages/admin/Users.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdminMenu from "../../components/AdminMenu";
 import Layout from "../../components/Layout";
+import axios from "axios";
 
 const Users = () => {
+  const [users, setUsers] = useState("");
+  const getAllUsers = async () => {
+    const { data } = await axios.get("/api/v1/user/getUser");
+    if (data?.success) {
+      setUsers(data?.users);
+      console.log(data);
+    }
+  };
+  useEffect(() => {
+    getAllUsers();
+  }, []);
   return (
     <Layout
       title={"all users page"}
@@ -13,7 +25,37 @@ const Users = () => {
           <div className="col-md-3">
             <AdminMenu />
           </div>
-          <div className="col-md-9">All users</div>
+          <div className="col-md-9">
+            <div className="user-list-page">
+              <h2 className="user-list-heading">All Users</h2>
+              <div className="user-list">
+                {users &&
+                  users?.map((user) => (
+                    <div
+                      key={user._id}
+                      className={`user-card ${
+                        user.role === 1 ? "admin-card" : ""
+                      }`}
+                    >
+                      <h3 className="user-name">{user.name}</h3>
+                      <p className="user-info">
+                        <span className="user-label">Address:</span>{" "}
+                        {user.address}
+                      </p>
+                      <p className="user-info">
+                        <span className="user-label">Email:</span> {user.email}
+                      </p>
+                      <p className="user-info">
+                        <span className="user-label">Phone:</span> {user.phone}
+                      </p>
+                      <p className="user-role">
+                        {user.role === 1 ? "Admin" : "Normal User"}
+                      </p>
+                    </div>
+                  ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
