c76d908fc0a5e893ff985b0be2f8979379588253
Yash Bhardwaj <86493014+yashB-2002@users.noreply.github.com>
2023-09-02 20:35:24 +0530

    Update UserMenu.js

--- frontend/src/components/UserMenu.js
+++ frontend/src/components/UserMenu.js
@@ -1,24 +1,11 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 
 const UserMenu = () => {
   return (
     <>
       <div className="text-center  ">
         <div className="list-group">
-          <h3>Admin Panel</h3>
-          <NavLink
-            to="/panel/user/orders"
-            className="list-group-item list-group-item-action"
-          >
-            Orders
-          </NavLink>
-          <NavLink
-            to="/panel/user/profile"
-            className="list-group-item list-group-item-action"
-          >
-            Profile
-          </NavLink>
+          <h3>User Panel</h3>
         </div>
       </div>
     </>
